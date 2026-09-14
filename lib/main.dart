43bdbc67304a502df21bcc1b631c7b01f778d3c4
mouhcine439 <mouhcine.tech05@gmail.com>
2026-09-14 15:27:28 +0100

    mise a jour

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: AppColors.bgColor,
       ),
       initialRoute: NameRoutes.splashScreen,
-      home: const ProfileScreen(),
+      home: const HomeScreen(),
       getPages: AppRoutes.pages,
     );
   }
